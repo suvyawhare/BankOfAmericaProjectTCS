38c718c1b5c9f7f3e37aa15a40aa35c95be7ed2a
positive waves <positivewaves.official@gmail.com>
2020-06-11 01:49:46 +0530

    Project Structure for bank of america project - Commiting

---
+++ BankOfAmericaTCS/src/main/java/com/sb/ex/BankOfAmericaTcsApplication.java
@@ -0,0 +1,13 @@
+package com.sb.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfAmericaTcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfAmericaTcsApplication.class, args);
+	}
+
+}
---
+++ BankOfAmericaTCS/src/test/java/com/sb/ex/BankOfAmericaTcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOfAmericaTcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
